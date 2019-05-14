6f42127bacf834801fe0b1dfb4ced7761c8a0f2f
zcribe <erlend.eelmets@gmail.com>
2019-05-14 16:17:15 +0300

    Add gitignore, add simple HTML wrapping

--- src/main.py
+++ src/main.py
@@ -8,27 +8,40 @@ class TextGenerator:
         self.seed = kalev_1850.KALEV
         self.seed_length = len(self.seed) - 1
 
-    def choose_pseudo_random_sentence(self):
+    def choose_sentence(self):
         return self.seed[randint(0, self.seed_length)]
 
-    def generate_paragraph(self, min_length=500):
+    def generate_paragraph(self, min_length=500, html=False, headline=False):
         paragraph = ""
 
         while len(paragraph) < min_length:
-            paragraph += self.choose_pseudo_random_sentence()
+            paragraph += self.choose_sentence()
+
+        if html:
+            paragraph = f"<p>{paragraph}</p>"
+
+        if headline:
+            line = self.choose_sentence()
+            if html:
+                paragraph = f"<h2>{line}</h2>{paragraph}"
+
+            else:
+                paragraph = f"{line}\n\n{paragraph}"
 
         return paragraph
 
-    def generate_text(self, nr_paragraphs=10):
+    def generate_text(self, paragraphs=10):
         text = ""
         counter = 0
 
-        while counter < nr_paragraphs:
-            text += self.generate_paragraph()
-            text += "\n"
+        while counter < paragraphs:
+            text = "\n\n".join([text, self.generate_paragraph()])
             counter += 1
 
         return text
 
+
+
+
 a = TextGenerator()
-print(a.generate_text())
+print(a.generate_paragraph(html=True, headline=True))
