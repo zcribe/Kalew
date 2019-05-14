92841a2f561c4d2d776b54ee8d633ccb45036d9f
zcribe <erlend.eelmets@gmail.com>
2019-05-14 11:53:41 +0300

    Sentence, Paragraph, Text creation implemented.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# Kalev
+![Kalev Icon](http://url/to/img.png)
+
+# Kalew
  Random text generator that creates vaguely Estonian looking text for placeholder or design purposes.
  It's main seed is digitized version of the epic Kalevipoeg from 1850 release.
+ 
+ Currently allows for simple sentence, paragraph, text creation.
--- src/main.py
+++ src/main.py
@@ -1,1 +1,34 @@
-from .seeds.kalev_1850 import KALEV
+from random import randint
+
+from seeds import kalev_1850
+
+
+class TextGenerator:
+    def __init__(self):
+        self.seed = kalev_1850.KALEV
+        self.seed_length = len(self.seed) - 1
+
+    def choose_pseudo_random_sentence(self):
+        return self.seed[randint(0, self.seed_length)]
+
+    def generate_paragraph(self, min_length=500):
+        paragraph = ""
+
+        while len(paragraph) < min_length:
+            paragraph += self.choose_pseudo_random_sentence()
+
+        return paragraph
+
+    def generate_text(self, nr_paragraphs=10):
+        text = ""
+        counter = 0
+
+        while counter < nr_paragraphs:
+            text += self.generate_paragraph()
+            text += "\n"
+            counter += 1
+
+        return text
+
+a = TextGenerator()
+print(a.generate_text())
